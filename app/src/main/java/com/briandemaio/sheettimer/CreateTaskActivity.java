ef6d6e6290124eddade22abc485e914eb270a20c
Brian De Maio <bdemaio1@gmail.com>
2019-08-25 09:10:15 -0400

    Added in changes for file saving after camera intent for task creation

--- app/src/main/java/com/briandemaio/sheettimer/CreateTaskActivity.java
+++ app/src/main/java/com/briandemaio/sheettimer/CreateTaskActivity.java
@@ -1,7 +1,9 @@
 package com.briandemaio.sheettimer;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -15,6 +17,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.io.ByteArrayOutputStream;
 
@@ -48,6 +52,31 @@ public class CreateTaskActivity extends AppCompatActivity {
                 }
             }
         });
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                int MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE = 1;
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_WRITE_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
         dispatchTakePictureIntent();
     }
 
